9aa611fbfcec12f3c5bd2213c95d08a13382b22e
Andiweli <stuermer.andreas@gmail.com>
2026-05-09 21:33:08 +0200

    Fixed teleport animation sequence

--- sources/app/src/main/cpp/ZGloom/gamelogic.cpp
+++ sources/app/src/main/cpp/ZGloom/gamelogic.cpp
@@ -1065,28 +1065,34 @@ bool GameLogic::Update(Camera* cam)
 			{
 				if (gmap->GetZones()[closestzone].ev > 1)
 				{
-					if (gmap->GetZones()[closestzone].ev == 24)
+					const uint32_t eventnum = gmap->GetZones()[closestzone].ev;
+					const bool levelExitEvent = (eventnum == 24);
+
+					if (levelExitEvent)
 					{
 						levelfinished = true;
 						SoundHandler::Play(SoundHandler::SOUND_TELEPORT);
 						playerobj.data.ms.pixsizeadd = 1;
 					}
 
-					if (!eventhit[gmap->GetZones()[closestzone].ev])
+					if (!eventhit[eventnum])
 					{
-						gmap->ExecuteEvent(gmap->GetZones()[closestzone].ev, gotele, tele);
+						// Event 24 is the Amiga level-exit trigger.  Execute its script,
+						// but do not allow a teleport command in that script to replace
+						// the slow blue beam-out with the normal fast teleport animation.
+						gmap->ExecuteEvent(eventnum, gotele, tele, !levelExitEvent);
 					}
 
 					// these are one-shot
-					if (gmap->GetZones()[closestzone].ev < 19)
+					if (eventnum < 19)
 					{
-						eventhit[gmap->GetZones()[closestzone].ev] = true;
+						eventhit[eventnum] = true;
 					}
 				}
 			}
 		}
 
-		if (gotele)
+		if (gotele && !levelfinished)
 		{
 			// teleport animation
 			activetele = tele;
